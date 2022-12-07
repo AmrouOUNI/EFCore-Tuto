b3016b1220b650c1ecc8272928858809ba5b3a4f
Amrou <amrou.ouni@gmail.com>
2022-12-07 07:47:03 +0100

    feat(model) : create initial model and migration

---
+++ EFCoreTuto.ConsoleApp/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTuto.ConsoleApp
+{
+    internal class BlogContext : DbContext
+    {
+
+        private static string connString = "Data Source=localhost; Initial Catalog=efcore_tuto; Integrated Security=SSPI;TrustServerCertificate=True";
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connString);
+        }
+
+    }
+}
